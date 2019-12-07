78785562d7716fcac57586b2d5c51bfe34e01a4f
Zachary Jacobs <skatboyza124@hotmail.com>
2019-12-07 21:26:20 +0000

    Done.

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -1,3 +1,38 @@
+require 'pry'
 def nyc_pigeon_organizer(data)
-  # write your code here!
+ 
+ result = {}
+ data[:gender].each{
+   |key,array| 
+   array.each{ |name|
+    result[name] = {
+      :color => [],
+      :gender => [key.to_s],
+      :lives => []
+    }
+   }
+   }
+data[:color].each { |color,values|
+values.each { |i|
+result[i][:color] << color.to_s
+ }
+}
+data[:lives].each { |lives,places|
+places.each { |i|
+result[i][:lives] << lives.to_s
+}
+}
+return result
 end
+ 
+ 
+ 
+
+#color_values = a.to_s
+#structure = {
+  #:color => color_values,
+  #:gender => a.to_s,
+ # :lives => a.to_s
+#}
+#if b.include?(i){ v.invert or result[i] = structure
+#}?
