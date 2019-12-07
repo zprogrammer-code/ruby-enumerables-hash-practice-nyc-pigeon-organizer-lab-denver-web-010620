669d25101ccbbdb7555c9dfffc0ea7eb700c9fa9
Zachary Jacobs <skatboyza124@hotmail.com>
2019-12-07 20:55:14 +0000

    Automatically backed up by Learn

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -9,19 +9,10 @@ if !result[i]
 result[i] = data[i]
 result[i] = {}
 
-color_values = a.to_s
-structure = {
-  :color => color_values,
-  :gender => a.to_s,
-  :lives => a.to_s
-}
-#if b.include?(i){ v.invert or result[i] = structure
-#}?
 end
  }
 }
 }
-binding.pry
 return result
 end
  
