1ed5d803a59786874692947909c340f49f442d92
Zachary Jacobs <skatboyza124@hotmail.com>
2019-12-07 20:54:38 +0000

    Automatically backed up by Learn

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -1,3 +1,38 @@
+require 'pry'
 def nyc_pigeon_organizer(data)
-  # write your code here!
+ 
+ result = {}
+data.each { |k,v,|
+v.each { |a,b|
+  b.each { |i|
+if !result[i]
+result[i] = data[i]
+result[i] = {}
+
+color_values = a.to_s
+structure = {
+  :color => color_values,
+  :gender => a.to_s,
+  :lives => a.to_s
+}
+#if b.include?(i){ v.invert or result[i] = structure
+#}?
 end
+ }
+}
+}
+binding.pry
+return result
+end
+ 
+ 
+ 
+
+#color_values = a.to_s
+#structure = {
+  #:color => color_values,
+  #:gender => a.to_s,
+ # :lives => a.to_s
+#}
+#if b.include?(i){ v.invert or result[i] = structure
+#}?
